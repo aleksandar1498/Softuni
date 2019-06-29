7d5e9f2f78d7f5b81fd60901a293fc2a351e45bb
aleksandar1498 <aleksandar.fs@gmail.com>
2019-06-29 07:45:06 +0300

    new changes performed in abstraction exercises

--- Exercises/Java_Advanced/Object Oriented Programming/Abstraction/Exercise/trafficLight/Main.java
+++ Exercises/Java_Advanced/Object Oriented Programming/Abstraction/Exercise/trafficLight/Main.java
@@ -1,22 +1,27 @@
 package trafficLight;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
         List<TrafficLight> lights=new ArrayList<>();
-        Arrays.stream(scanner.nextLine().split("\\s+")).forEach(l -> lights.add(new TrafficLight(Light.valueOf(l))));
-        int numChanges=Integer.parseInt(scanner.nextLine());
+        Arrays.stream(reader.readLine().split("\\s+")).forEach(l -> lights.add(new TrafficLight(Light.valueOf(l))));
+        int numChanges=Integer.parseInt(reader.readLine());
+        StringBuilder builder=new StringBuilder();
         for (int i = 0; i < numChanges; i++) {
             for (TrafficLight trafficLight : lights) {
                 trafficLight.changeLight();
-                System.out.print(trafficLight+" ");
+                builder.append(trafficLight).append(" ");
             }
-            System.out.println();
+            System.out.println(builder.toString());
+            builder.delete(0,builder.length());
         }
 
 

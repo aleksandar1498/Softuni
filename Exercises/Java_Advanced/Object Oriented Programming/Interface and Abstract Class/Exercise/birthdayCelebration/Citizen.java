443f7c13c55c76c6ae89c86db96e1f24bfc05701
aleksandar1498 <aleksandar.fs@gmail.com>
2019-07-09 20:31:34 +0300

    new exercises with Interfaces

---
+++ Exercises/Java_Advanced/Object Oriented Programming/Interface and Abstract Class/Exercise/birthdayCelebration/Citizen.java
@@ -0,0 +1,36 @@
+package birthdayCelebration;
+
+
+public class Citizen implements Person, Identifiable, Birthable {
+    private String name;
+    private int age;
+    private String birthDate;
+    private String id;
+
+    public Citizen(String name, int age, String id, String birthDate) {
+        this.name = name;
+        this.age = age;
+        this.birthDate = birthDate;
+        this.id = id;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public String getBirthDate() {
+        return this.birthDate;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+}
---
+++ Exercises/Java_Advanced/Object Oriented Programming/Interface and Abstract Class/Exercise/birthdayCelebration/Identifiable.java
@@ -0,0 +1,6 @@
+package birthdayCelebration;
+
+public interface Identifiable {
+   String getId();
+
+}

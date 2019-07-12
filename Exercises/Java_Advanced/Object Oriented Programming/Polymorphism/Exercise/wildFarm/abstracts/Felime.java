d19b39474004665a96626fd6b67116f28c776c0a
aleksandar1498 <aleksandar.fs@gmail.com>
2019-07-12 12:08:40 +0300

    New Exercises with polymorphism

---
+++ Exercises/Java_Advanced/Object Oriented Programming/Polymorphism/Exercise/wildFarm/abstracts/Felime.java
@@ -0,0 +1,8 @@
+package wildFarm.abstracts;
+
+public abstract class Felime extends Mammal{
+
+    public Felime(String animalName, String animalType, Double animalWeight, String livingRegion) {
+        super(animalName, animalType, animalWeight, livingRegion);
+    }
+}
---
+++ Exercises/Java_Advanced/Object Oriented Programming/Polymorphism/Exercise/wildFarm/models/Meat.java
@@ -0,0 +1,9 @@
+package wildFarm.models;
+
+import wildFarm.abstracts.Food;
+
+public class Meat extends Food {
+    public Meat(Integer quantity) {
+        super(quantity);
+    }
+}

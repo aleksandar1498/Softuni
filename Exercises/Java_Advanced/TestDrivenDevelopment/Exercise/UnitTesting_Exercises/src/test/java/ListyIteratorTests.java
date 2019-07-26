4faca74aaf54d2c8cc3341977b7a667e3fa0e14c
aleksandar1498 <aleksandar.fs@gmail.com>
2019-07-26 08:56:35 +0300

    JUnit Testing

---
+++ Exercises/Java_Advanced/TestDrivenDevelopment/Exercise/UnitTesting_Exercises/src/test/java/ListyIteratorTests.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+import p03_IteratorTest.ListIterator;
+
+import javax.naming.OperationNotSupportedException;
+
+import static org.junit.Assert.assertEquals;
+
+public class ListyIteratorTests {
+    public static final String [] BASE_LIST_TO_ITERATE= {"A","B","C"};
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void constructorShouldThrowExpectedExceptionIfNullArgumentIsPassed() throws OperationNotSupportedException {
+        ListIterator listIterator = new ListIterator(null);
+    }
+    @Test
+    public void moveShouldReturnFalseIfTheCurrentIndexIsPointingAtTheEnd() throws OperationNotSupportedException {
+        ListIterator listIterator = new ListIterator(BASE_LIST_TO_ITERATE);
+        listIterator.move();
+        listIterator.move();
+        boolean result = listIterator.move();
+        assertEquals(false,result);
+    }
+    @Test
+    public void moveShouldReturnTrueIfTheCurrentIndexIsNotPointingAtTheEnd() throws OperationNotSupportedException {
+        ListIterator listIterator = new ListIterator(BASE_LIST_TO_ITERATE);
+        listIterator.move();
+        boolean result = listIterator.move();
+        assertEquals(true,result);
+    }
+
+    @Test
+    public void hasNextShouldReturnFalseIfTheCurrentPointingIndexIsTheLast() throws OperationNotSupportedException {
+        ListIterator listIterator = new ListIterator(BASE_LIST_TO_ITERATE);
+        listIterator.move();
+        listIterator.move();
+        boolean result = listIterator.hasNext();
+        assertEquals(false,result);
+    }
+    @Test
+    public void hasNextShouldReturnTrueIfTheCurrentPointingIndexIsNotTheLast() throws OperationNotSupportedException {
+        ListIterator listIterator = new ListIterator(BASE_LIST_TO_ITERATE);
+        listIterator.move();
+        boolean result = listIterator.hasNext();
+        assertEquals(true,result);
+    }
+}

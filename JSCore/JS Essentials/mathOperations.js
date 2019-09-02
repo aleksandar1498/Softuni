5a50a3ae3e7f0f288c0b7e2060799f33baa6c847
aleksandar1498 <aleksandar.fs@gmail.com>
2019-09-02 20:15:42 +0300

    Math Operations exercise completed

---
+++ JSCore/JS Essentials/mathOperations.js
@@ -0,0 +1,23 @@
+
+function solveOperation(a,b,operator){
+	switch(operator){
+		case "+":
+			console.log(a+b);
+		break;
+		case "-":
+			console.log(a-b);
+		break;
+		case "*":
+			console.log(a*b);
+		break;
+		case "/":
+			console.log(a/b);
+		break;
+		case "%":
+			console.log(a%b);
+		break;
+		case "**":
+			console.log(a**b);
+		break;
+	}	
+}

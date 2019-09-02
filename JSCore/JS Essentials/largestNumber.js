bb552e175eade3e7100d820a05875987582c9a9f
aleksandar1498 <aleksandar.fs@gmail.com>
2019-09-02 20:54:15 +0300

    square of stars exercise completed

---
+++ JSCore/JS Essentials/largestNumber.js
@@ -0,0 +1,4 @@
+function getLargest(a,b,c){
+	let result = Math.max(a,b,c);
+	console.log(`The largest number is ${result}.`);
+}
---
+++ JSCore/JS Essentials/squareOfStars.js
@@ -0,0 +1,5 @@
+function square(size){
+    for(let i = 0; i<size; i++){
+        console.log('*'.repeat(size).split('').join(' '));
+		}
+}

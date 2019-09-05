89573cda8814ee9de580604c0d0de94b940736a3
aleksandar1498 <aleksandar.fs@gmail.com>
2019-09-05 20:43:46 +0300

    Refactor Exercise Completed

--- JSCore/JS Essentials/DomAndObjects/Lab/06. Format the Text/solution.js
+++ JSCore/JS Essentials/DomAndObjects/Lab/06. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO...
+ let content = document.getElementById("input");
+ let output = document.getElementById("output");
+ let sentences = content.innerHTML.replace(/\.$/g,"").split(/\./).filter(sentence => sentence.length > 0);
+ console.log(sentences);
+ for(let i=0;i < sentences.length;i+=3){
+	 let paragraph = document.createElement("p");
+	 if(i+2 < sentences.length){
+		paragraph.innerHTML = sentences.slice(i,i+3);
+	 }else{
+		paragraph.innerHTML = sentences.slice(i,sentences.length);
+	 }
+	 output.appendChild(paragraph);
+ }
+
 }

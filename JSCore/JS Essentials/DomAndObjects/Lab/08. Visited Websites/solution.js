1099d97839ff57810d70b1f43d855789a3e1e4f3
aleksandar1498 <aleksandar.fs@gmail.com>
2019-09-06 17:57:00 +0300

    Count click Exercise completed

--- JSCore/JS Essentials/DomAndObjects/Lab/08. Visited Websites/solution.js
+++ JSCore/JS Essentials/DomAndObjects/Lab/08. Visited Websites/solution.js
@@ -1,4 +1,14 @@
 function solve() {
-
-  //TODO...
+	let links = document.getElementsByClassName("link-1");
+	for(let i = 0 ; i < links.length ; i++){
+		links[i].addEventListener("click", function() {
+		
+		let p = this.getElementsByTagName("p")[0];
+		let count = Number(p.textContent.replace(/[^0-9]+/gi,""));
+		count++;
+		p.textContent = p.textContent.replace(/[0-9]+/gi,count+"");
+		
+	});
+	}
+	
 }

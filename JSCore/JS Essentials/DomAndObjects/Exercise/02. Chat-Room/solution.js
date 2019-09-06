4d67b0a6335ba0e3e0efba6757a9328c7d8fccd8
aleksandar1498 <aleksandar.fs@gmail.com>
2019-09-06 20:49:29 +0300

    Chat box Exercise completed

--- JSCore/JS Essentials/DomAndObjects/Exercise/02. Chat-Room/solution.js
+++ JSCore/JS Essentials/DomAndObjects/Exercise/02. Chat-Room/solution.js
@@ -1,5 +1,12 @@
-function solve() {
-   //TODO...
+	function solve() {
+   document.getElementById("send").addEventListener('click' , function (){
+	   let divElement = document.createElement("div");
+	   divElement.className = 'message my-message';
+	   divElement.innerText = document.getElementById("chat_input").value;
+	   console.log(divElement);
+	   document.getElementById("chat_messages").append(divElement);
+	   document.getElementById("chat_input").value = ""
+   });
 }
 
 

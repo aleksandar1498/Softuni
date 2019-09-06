c32891a9753cd0cb95038378b2f847d9bc4e8ab0
aleksandar.fs@gmail.com <aleksandar.fs@gmail.com>
2019-09-06 11:23:08 +0300

    New Exercises completed

--- JSCore/JS Essentials/DomAndObjects/Lab/07. Growing Word/solution.js
+++ JSCore/JS Essentials/DomAndObjects/Lab/07. Growing Word/solution.js
@@ -1,4 +1,28 @@
-function growingWord() {
 
-  //TODO...
+
+function growingWord() {
+  let colorsEnum = {"blue":0,"green":1,"red":2};
+  let colors = document.getElementById("colors").getElementsByTagName("div");
+  let growingWord = document.getElementById("exercise").getElementsByTagName("p")[0];
+  if(!growingWord.style.color){
+    growingWord.style.color = colors[colorsEnum["blue"]].innerHTML.toLowerCase();
+  }else{
+    let currentIndex = colorsEnum[growingWord.style.color];
+    currentIndex++;
+    if(currentIndex >= colors.length){
+      currentIndex = 0;
+    }
+    growingWord.style.color = colors[currentIndex].innerHTML.toLowerCase();
+  }
+  
+  let fontSize = growingWord.style.fontSize;
+  if(fontSize == 0 ){
+    growingWord.style.fontSize = 2+"px";
+  }else{
+    growingWord.style.fontSize = fontSize*2+"px";
+  }
+
+  
+  
+  
 }

5b700c366becdc3195ce30378c416c02842bcb94
aleksandar.fs@gmail.com <aleksandar.fs@gmail.com>
2019-11-02 13:13:18 +0200

    Functional Programming + JUnit

--- Java_Web/Functional/rotator/src/main/java/com/dxc/internship/Rotator.java
+++ Java_Web/Functional/rotator/src/main/java/com/dxc/internship/Rotator.java
@@ -12,19 +12,33 @@ public final class Rotator {
 	private Rotator() {
 		// Utility class
 	}
+
+	public static void main(String[] args) {
+
+		Rotator.revertInternWords(new char[] { ' ', ' ', ' ' });
+		Rotator.revertInternWords(
+				new char[] { 'I', ' ', 'A', 'm', ' ', 'H', 'e', 'l', 'l', 'o', ' ', ' ', 'I', 'v', '!', 'a', 'n' });
+		Rotator.revertInternWords(new char[] { 'I', ' ', 'A', 'm', ' ', 'H', 'e', 'l', 'l', 'o', ' ', ' ', 'I', 'v',
+				'!', 'a', 'n', ' ', ' ' });
+		Rotator.revertInternWords(new char[] { ' ', 'I', ' ', 'A', 'm', ' ', 'H', 'e', 'l', 'l', 'o', ' ', ' ', 'I',
+				'v', '!', 'a', 'n', ' ', ' ' });
+	}
+
 	/**
-	 * @param word is the char array that needs to be reverted. If it is null or empty array an IllegalArgumentExceptionIsThrown
+	 * Reverts an array of chars from {@code start} index  to {@code end} index 
+	 * 
+	 * @param word  is the char array that needs to be reverted. If it is null or
+	 *              empty array an IllegalArgumentExceptionIsThrown
 	 * @param start is the starting index for the reversion . It is inclusive
-	 * @param end is the ending index for the reversion. It is inclusive
-	 * @throws IllegalArgumentException if :
-	 *  -word is null or empty
-	 *  -start index is less then 0 or more then the length of the word 
-	 *  -end index is less then 0 or more then the length of the word 
-	 *  -start index is greater than end index
+	 * @param end   is the ending index for the reversion. It is inclusive
+	 * @throws IllegalArgumentException if : -word is null or empty -start index is
+	 *                                  less then 0 or more then the length of the
+	 *                                  word -end index is less then 0 or more then
+	 *                                  the length of the word -start index is
+	 *                                  greater than end index
 	 */
 	public static final void revert(char[] word, int start, int end) {
 		checkValidityArray(word);
-		
 		if (start < 0 || start >= word.length || end < 0 || end >= word.length) {
 			throw new IllegalArgumentException("index is out of bounds");
 		}
@@ -32,6 +46,7 @@ public final class Rotator {
 			throw new IllegalArgumentException("start cannot be after the end");
 		}
 		int middle = start + (end - start) / 2;
+		String.copyValueOf(word);
 		while (start <= middle) {
 			char temp = word[start];
 			word[start] = word[end];
@@ -40,17 +55,55 @@ public final class Rotator {
 			end--;
 		}
 	}
+
 	/**
-	 * @param word is the char array that needs to be reverted
-	 * this method implements polymorphism, calls the original implementation of the method by setting the starting and ending value {@link #revert(char[], int, int) revert}
+	 * Reverts an array of chars
+	 * 
+	 * @param word is the char array that needs to be reverted this method
+	 *             implements polymorphism, calls the original implementation of the
+	 *             method by setting the starting and ending value
+	 *             {@link #revert(char[], int, int) revert}
 	 */
 	public static final void revert(char[] word) {
 		checkValidityArray(word);
-		
 		Rotator.revert(word, 0, word.length - 1);
 	}
-	private static void checkValidityArray(char [] word) {
-		if(word == null) {
+
+	/**
+	 * Reverts the order of the whole words in the array of char ,maintaining the number and the order of the spaces
+	 * @param word is the char array that needs to be reverted
+	 * @throws  IllegalArgumentException
+     *          If {@code word} is null,or {@code word} is empty
+	 */
+	public static final void revertInternWords(char[] word) {
+		revert(word);
+		int startIndex = -1;
+		for (int i = 0; i < word.length; i++) {
+
+			if (startIndex == -1 && word[i] != ' ') {
+				startIndex = i;
+			} else if (startIndex != -1 && word[i] == ' ') {
+				revert(word, startIndex, i - 1);
+				startIndex = -1;
+			}
+
+		}
+
+		if (startIndex != -1) {
+			revert(word, startIndex, word.length - 1);
+		}
+
+		System.out.println(word);
+	}
+
+	/**
+	 * Utility method used to check if the passed argument is valid 
+	 * @param word is the argument that needs to be checked
+	 * @throws IllegalArgumentException 
+	 * 	if the passed argument is null or the passed argument length is equal to '0'
+	 */
+	private static void checkValidityArray(char[] word) {
+		if (word == null) {
 			throw new IllegalArgumentException("word cannot be null");
 		}
 		if (word.length == 0) {
--- Java_Web/Functional/rotator/src/test/java/com/dxc/internship/RotatorTest.java
+++ Java_Web/Functional/rotator/src/test/java/com/dxc/internship/RotatorTest.java
@@ -2,36 +2,67 @@ package com.dxc.internship;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class RotatorTest {
-	static final char [] WORD = {'a','b','c','d','e'}; 
+	static final char[] WORD = { 'a', 'b', 'c', 'd', 'e' };
 	static final int WORD_LENGTH = 5;
+
 	@Test
-	void rotateShouldThrowExpectedIfNullArrayIsPassed() {
-		testInvalidData((a) -> Rotator.revert((char[])a), null, "word cannot be null");
+	void revertShouldWorkWithOddLengthArray() {
+		char[] original = "ab".toCharArray();
+		char[] expected = "ba".toCharArray();
+		Rotator.revert(original);
+		assertTrue(Arrays.equals(original, expected));
+	}
+
+	@ParameterizedTest(name = "{0} reversed should be {1}")
+	@MethodSource("evenLengthInput")
+	void revertShouldWorkWithEvenLengthArray(String input,String expected) {
+		char[] original = input.toCharArray();
+		char[] result = expected.toCharArray();
+		Rotator.revert(original);
+		assertTrue(Arrays.equals(original, result));
+	}
+
+	final static Stream<Arguments> evenLengthInput(){
+		return Stream.of(
+			Arguments.arguments("a","a"),
+			Arguments.arguments("abc","cba"),
+			Arguments.arguments("abcde","edcba")
+		);
 	}
 
 	@Test
-	void rotateShouldThrowExpectedIfEmptyArrayIsPassed() {
-		testInvalidData((a) -> Rotator.revert((char[])a), new char[0], "word cannot be empty");
+	void revertShouldThrowExpectedIfNullArrayIsPassed() {
+		testInvalidData((a) -> Rotator.revert((char[]) a), null, "word cannot be null");
 	}
-	
-	@ParameterizedTest
-	@ValueSource(ints = {-1,-2,Integer.MAX_VALUE,Integer.MIN_VALUE,WORD_LENGTH})
-	void rotateShouldThrowExpectedIfInvalidStartIndexIsPassed(int startIndex) {
-		testInvalidData((a) -> Rotator.revert(WORD,(int)a,WORD.length),startIndex,"index is out of bounds");
+
+	@Test
+	void revertShouldThrowExpectedIfEmptyArrayIsPassed() {
+		testInvalidData((a) -> Rotator.revert((char[]) a), new char[0], "word cannot be empty");
 	}
+
 	@ParameterizedTest
-	@ValueSource(ints = {-1,-2,Integer.MAX_VALUE,Integer.MIN_VALUE,WORD_LENGTH})
+	@ValueSource(ints = { -1, -2, Integer.MAX_VALUE, Integer.MIN_VALUE, WORD_LENGTH })
+	void revertShouldThrowExpectedIfInvalidStartIndexIsPassed(int startIndex) {
+		testInvalidData((a) -> Rotator.revert(WORD, (int) a, WORD.length), startIndex, "index is out of bounds");
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { -1, -2, Integer.MAX_VALUE, Integer.MIN_VALUE, WORD_LENGTH })
 	void rotateShouldThrowExpectedIfInvalidEndIndexIsPassed(int endIndex) {
-		testInvalidData((a) -> Rotator.revert(WORD,WORD.length,(int)a),endIndex,"index is out of bounds");
+		testInvalidData((a) -> Rotator.revert(WORD, WORD.length, (int) a), endIndex, "index is out of bounds");
 	}
-	
+
 	private void testInvalidData(final Consumer<Object> func, Object arg, String expectedMessage) {
 		IllegalArgumentException exc = assertThrows(IllegalArgumentException.class, () -> {
 			func.accept(arg);
